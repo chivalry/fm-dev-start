045c0c8b573a83947243b22ef3518a2a8cb6ae25
Charles Ross <chivalry@mac.com>
2018-07-25 16:38:52 -0700

    commit from script

---
+++ commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fmsadmin backup fm-dev-start.fmp12 --dest filemac:/Aslan/Users/chuck/Projects/chivalry/fm-dev-start/ --username Administrator --password "eyeWi110fourg3t" -k 0
+mv Databases/fm-dev-start.fmp12 ./
+rm -r Databases
+
+git add .
+git commit -a -m "commit from script"
+git push
