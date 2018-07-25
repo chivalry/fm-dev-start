76504e31824a57eea303e8a88bafbd1f1d7eb0c3
Charles Ross <chivalry@mac.com>
2018-07-25 16:51:05 -0700

    Remove commit script

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fmsadmin backup fm-dev-start.fmp12 --dest filemac:/Aslan/Users/chuck/Projects/chivalry/fm-dev-start/ --username Administrator --password "eyeWi110fourg3t" -k 0
-mv Databases/fm-dev-start.fmp12 ./
-rm -r Databases
-
-git add .
-git commit -a -m "Add standard script folders."
-git push
